f90db47a21a669c4884ff9f15d573de6c248eaba
Jun Kang <jun@ip-172-31-0-245.ap-northeast-1.compute.internal>
2015-10-05 06:56:28 +0000

    only post-accession survival

--- a.R
+++ a.R
@@ -8,11 +8,148 @@ summary(data)
 data$Name <- as.character(data$Name)
 data$UV.signature
 data$MUTATIONSUBTYPES
+data$NECROSIS
 
 data$Name[duplicated(gsub('-0[16]{1}$', '', data$Name))]
 
 data <- data[data$Name != "TCGA-ER-A19T-06",]
 data <- data[data$Name != "TCGA-ER-A2NF-06",]
+
+data$chromothripsis [data$SHATTERSEEK_Chromothripsis_calls == 'negative'] <- 'negative'
+data$chromothripsis [data$SHATTERSEEK_Chromothripsis_calls != 'negative'] <- 'positive'
+data$chromothripsis [is.na(data$SHATTERSEEK_Chromothripsis_calls)] <- NA
+data$chromothripsis <- factor(data$chromothripsis)
+
+data$PIGMENT.SCORE.integ <- as.integer(data$PIGMENT.SCORE)
+data$LYMPHOCYTE.DISTRIBUTION.integ <- as.integer(data$LYMPHOCYTE.DISTRIBUTION)
+data$LYMPHOCYTE.DENSITY.integ <- as.integer(data$LYMPHOCYTE.DENSITY)
+data$LYMPHOCYTE.SCORE.integ <- as.integer(data$LYMPHOCYTE.SCORE)
+
+
+data$PIGMENT.SCORE.f <- factor(data$PIGMENT.SCORE)
+data$LYMPHOCYTE.DISTRIBUTION.f <- factor(data$LYMPHOCYTE.DISTRIBUTION)
+data$LYMPHOCYTE.DENSITY.f <- factor(data$LYMPHOCYTE.DENSITY)
+data$LYMPHOCYTE.SCORE.f <- factor(data$LYMPHOCYTE.SCORE)
+data$necrosis <- factor(data$NECROSIS >0 )
+
+########################## TSS #################################]
+
+setwd('/home/jun/melanoma_UV/')
+read.delim('tissueSourceSite.txt') -> tss
+read.csv('tss.txt') -> location
+
+data$ID <- gsub('-..$', '' , data$Name)
+tss[tss$Study.Name == 'Skin Cutaneous Melanoma' , 1:2] -> tss_melanoma
+data$tss_ID = as.factor(unlist(strsplit(as.character(data$ID) , '-'))[seq(2,3*dim(data)[1],3)])
+data$tss = factor (data$tss_ID, 
+                       levels = tss_melanoma[tss_melanoma$TSS.Code %in%  data$tss_ID, ]$TSS.Code,
+                       labels = tss_melanoma[tss_melanoma$TSS.Code %in%  data$tss_ID, ]$Source.Site)
+
+data = merge(data, location, by.x = 'tss', by.y = 'Institute', all.x = TRUE)
+
+data$Latitude = round(abs(data$latitude), 0)
+
+
+##################### Lentigerous #######################
+
+setwd('/home/jun/melanoma_UV')
+histologic_type <- read.delim('MELANOMA.txt')
+histologic_type$ID <- paste('TCGA-', 
+                            gsub('-[A-Z0-9]{3}-[0-9]{2}-[A-Z0-9]{3}', '', histologic_type$ID),
+                            sep = '')
+summary(histologic_type)
+
+data <- merge(histologic_type, data, by = 'ID', all.y =TRUE)
+
+t(t(colnames(data)))
+
+####################################################################################
+
+library(compareGroups)
+
+compareGroups(UV.signature~ 
+                CURATED_AGE_AT_INITIAL_PATHOLOGIC_DIAGNOSIS +
+                CURATED_AGE_AT_TCGA_SPECIMEN +
+                GENDER+
+                Latitude+
+                CURATED_SITE_OF_PRIMARY_TUMOR_KNOWN_PRIMARY_ONLY +
+                REGIONAL_VS_PRIMARY+
+                type + 
+                CURATED_BRESLOW +
+                CURATED_ULCERATION +
+                PIGMENT.SCORE.integ +
+                CURATED_PATHOLOGIC_STAGE_AJCC7_AT_DIAGNOSIS_SIMPLE +
+                MUTATIONSUBTYPES+
+                NECROSIS+
+                necrosis +
+                Tumour.content........nuceli.that.are.tumour.cells......0.100..+                         
+                LYMPHOCYTE.DISTRIBUTION.integ+                                                                 
+                LYMPHOCYTE.DENSITY.integ+                                                                      
+                LYMPHOCYTE.SCORE.integ+                                                                      
+                PURITY..ABSOLUTE.+                                                                      
+                PLOIDY..ABSOLUTE.+                                                                       
+                #TOTAL.MUTATIONS+                                                                         
+                #UV.RATE+
+                chromothripsis ,
+              data = data) -> table
+
+table(data$Pathologic_stage, data$uv_g)
+
+createTable(table) -> table
+
+setwd('/home/jun/UV_git/')
+export2csv(table, 'table.csv')
+
+compareGroups(  ~ 
+                CURATED_AGE_AT_INITIAL_PATHOLOGIC_DIAGNOSIS +
+                CURATED_AGE_AT_TCGA_SPECIMEN +
+                GENDER+
+                Latitude+
+                CURATED_SITE_OF_PRIMARY_TUMOR_KNOWN_PRIMARY_ONLY +
+                REGIONAL_VS_PRIMARY+
+                type + 
+                CURATED_BRESLOW +
+                CURATED_ULCERATION +
+                PIGMENT.SCORE.integ +
+                CURATED_PATHOLOGIC_STAGE_AJCC7_AT_DIAGNOSIS_SIMPLE +
+                MUTATIONSUBTYPES+
+                NECROSIS+
+                necrosis +
+                Tumour.content........nuceli.that.are.tumour.cells......0.100..+                         
+                LYMPHOCYTE.DISTRIBUTION.integ+                                                                 
+                LYMPHOCYTE.DENSITY.integ+                                                                      
+                LYMPHOCYTE.SCORE.integ+                                                                      
+                PURITY..ABSOLUTE.+                                                                      
+                PLOIDY..ABSOLUTE.+                                                                       
+                #TOTAL.MUTATIONS+                                                                         
+                #UV.RATE+
+                chromothripsis ,
+              data = data[is.na(data$UV.signature) == FALSE,]) -> table_all
+
+table(data$Pathologic_stage, data$uv_g)
+
+createTable(table_all) -> table_all
+
+setwd('/home/jun/UV_git/')
+export2csv(table_all, 'table_all.csv')
+
+
+chisq.test(data[is.na(data$UV.signature) == FALSE,]$necrosis, data[is.na(data$UV.signature) == FALSE,]$UV.signature)
+wilcox.test(CURATED_BRESLOW~UV.signature, data)
+wilcox.test(PIGMENT.SCORE.integ~UV.signature, data)
+wilcox.test(LYMPHOCYTE.DISTRIBUTION.integ~UV.signature, data)
+wilcox.test(LYMPHOCYTE.DENSITY.integ~UV.signature, data)
+wilcox.test(LYMPHOCYTE.SCORE.integ~UV.signature, data)
+wilcox.test(as.numeric(data$CURATED_PATHOLOGIC_STAGE_AJCC7_AT_DIAGNOSIS_SIMPLE)~
+              data$UV.signature)
+
+
+tapply(data$NECROSIS,data$UV.signature, function(x) median(x, na.rm = T) )
+hist(data$LYMPHOCYTE.DISTRIBUTION)
+hist(data$LYMPHOCYTE.DENSITY)
+hist(data$LYMPHOCYTE.SCORE)
+
+t(t(colnames(data)))
 
 ID_BRAF <- data$Name[data$MUTATIONSUBTYPES == 'BRAF_Hotspot_Mutants']
 ID_RAS <- data$Name[data$MUTATIONSUBTYPES == 'RAS_Hotspot_Mutants']
@@ -101,28 +238,6 @@ CCtoTT <- subst %in% c('CC>TT', 'GG>AA')
 sum((CtoT+GtoA+CCtoTT) > 0)
 
 
-####################################################################################
-
-library(compareGroups)
-
-compareGroups(UV.signature~ 
-                CURATED_AGE_AT_INITIAL_PATHOLOGIC_DIAGNOSIS +
-                CURATED_AGE_AT_TCGA_SPECIMEN +
-                GENDER+
-                CURATED_SITE_OF_PRIMARY_TUMOR_KNOWN_PRIMARY_ONLY +
-                REGIONAL_VS_PRIMARY+
-                CURATED_BRESLOW +
-                CURATED_ULCERATION +
-                PIGMENT.SCORE+
-                CURATED_PATHOLOGIC_STAGE_AJCC7_AT_DIAGNOSIS_SIMPLE +
-                MUTATIONSUBTYPES,
-              data = data) -> table
-
-table(data$Pathologic_stage, data$uv_g)
-
-createTable(table) -> table
-export2csv(table, 'table.csv')
-
 
 ############################  survival ############################# 
 library(survival)
@@ -145,10 +260,50 @@ data_late <- subset(data, stage == 'Stage III-IV')
 strata = levels(data$UV.signature)
 
 library(Cairo)
-CairoSVG(file = "Survival.svg",  width = 10, height = 15, 
+
+CairoSVG(file = "Survival.svg",  width = 5, height = 5, 
          onefile = TRUE, bg = "transparent",
          pointsize = 12)
-par(mfrow = c(3,2), mar=c(6,4,2,8), mgp = c(2, 1, 0))
+par(
+  #mfrow = c(1,2), 
+  mar=c(6,4,2,6), mgp = c(2, 1, 0))
+
+
+fit = npsurv(Surv(TCGA_M, CURATED_VITAL_STATUS == 'Dead')~
+               UV.signature, data = data)
+fit
+
+diff = survdiff(Surv(TCGA_M, CURATED_VITAL_STATUS == 'Dead')~
+                  UV.signature, data = data)
+diff
+
+survplot(fit,
+         time.inc = 12,
+         xlab = 'Months',
+         lty = c(1:3),
+         conf="none", add=FALSE, 
+         label.curves=FALSE, abbrev.label=FALSE,
+         levels.only=TRUE, lwd=par('lwd'),
+         col=1, col.fill=gray(seq(.95, .75, length=5)),
+         loglog=FALSE,n.risk=TRUE,logt=FALSE,
+         dots=FALSE,
+         grid=FALSE,
+         srt.n.risk=0, sep.n.risk=0.04, adj.n.risk=0.3, 
+         y.n.risk=-0.3, cex.n.risk=0.6, pr=FALSE       
+)
+
+legend(60, 1.0, strata, lty = c(1:3), cex = 0.8,
+       xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
+       trace = TRUE,
+       bty = 'n')
+
+legend(55, 0.87, 'P-value = 0.081', cex = 0.8,
+       xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
+       trace = TRUE,
+       bty = 'n')
+
+dev.off()
+
 
 fit = npsurv(Surv(OS_M, CURATED_VITAL_STATUS == 'Dead')~
                UV.signature, data = data)
@@ -187,45 +342,6 @@ legend(5.5*12, 0.8, 'All OA', cex = 1,
        xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
        trace = TRUE,
        bty = 'n')
-
-fit = npsurv(Surv(TCGA_M, CURATED_MELANOMA_SPECIFIC_VITAL_STATUS..0....ALIVE.OR.CENSORED...1....DEAD.OF.MELANOMA.. == 1)
-             ~ UV.signature, data = data)
-fit
-
-diff = survdiff(Surv(TCGA_M, CURATED_MELANOMA_SPECIFIC_VITAL_STATUS..0....ALIVE.OR.CENSORED...1....DEAD.OF.MELANOMA.. == 1)
-                ~ UV.signature, data = data)
-diff
-
-survplot(fit,
-         time.inc = 12,
-         xlab = 'Months',
-         lty = c(1:3),
-         conf="none", add=FALSE, 
-         label.curves=FALSE, abbrev.label=FALSE,
-         levels.only=TRUE, lwd=par('lwd'),
-         col=1, col.fill=gray(seq(.95, .75, length=5)),
-         loglog=FALSE,n.risk=TRUE,logt=FALSE,
-         dots=FALSE,
-         grid=FALSE,
-         srt.n.risk=0, sep.n.risk=0.04, adj.n.risk=0.5, 
-         y.n.risk=-0.25, cex.n.risk=0.6, pr=FALSE       
-)
-
-legend(60, 1.0, strata, lty = c(1:3), cex = 0.8,
-       xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
-       trace = TRUE,
-       bty = 'n')
-
-legend(5.5*12, 0.85, 'P-value = 0.33', cex = 0.8,
-       xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
-       trace = TRUE,
-       bty = 'n')
-
-legend(5.5*12, 0.8, 'All TCGA', cex = 1,
-       xjust = 0, yjust = 1, x.intersp = 1, y.intersp = 1,
-       trace = TRUE,
-       bty = 'n')
-
 ################## Early Stage ##############################
 fit = npsurv(Surv(OS_M, CURATED_VITAL_STATUS == 'Dead')~
                UV.signature, data = data_early)
